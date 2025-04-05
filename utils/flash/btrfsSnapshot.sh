htot/meta-intel-edison

commit 42556be07a32a742694c6e23062d29de60e151ac

postbuild: fix image names now include rootfs Yocto Nanbield changed the name of the resulting image from edison-image-edison to edison-image-edison.rootfs.
Signed-off-by: Ferry Toth <ftoth@exalondelft.nl>

utils/flash/btrfsSnapshot.sh
@@ -7,7 +7,7 @@
 
 dirs="bin boot dev etc home lib media mnt opt proc run sbin sketch sys tmp usr var"
 deploy_dir=tmp/deploy/images/edison
-image_name=edison-image-edison
+image_name=edison-image-edison.rootfs
 image_ext=btrfs
 initrd=core-image-minimal-initramfs-edison.cpio.gz
 
@@ -76,7 +76,7 @@ if [ -d tmpbtrfs/@new ]; then
 fi
 
 # btrfs compress the snapshot
-sudo 7za a ${deploy_dir}/edison-image-edison.snapshot.7z ${deploy_dir}/edison-image-edison.snapshot
+sudo 7za a ${deploy_dir}/${image_name}.snapshot.7z ${deploy_dir}/${image_name}.snapshot
 
 # btrfs delete the snapshot
 sudo rm ${deploy_dir}/${image_name}.snapshot

utils/flash/postBuild.sh
@@ -27,21 +27,21 @@ else
 fi
 
 # Get edison rootfs image size
-IMAGE_SIZE_MB=$((`stat --printf="%s" -L $build_dir/tmp/deploy/images/edison/edison-image-edison.ext4` / 1048576))
+IMAGE_SIZE_MB=$((`stat --printf="%s" -L $build_dir/tmp/deploy/images/edison/edison-image-edison.rootfs.ext4` / 1048576))
 
 echo "EDISON_ROOTFS_MB = $EDISON_ROOTFS_MB, IMAGE_SIZE_MB = $IMAGE_SIZE_MB"
 
 # Compare rootfs partition settings with rootfs image
 if [ $EDISON_ROOTFS_MB -lt $IMAGE_SIZE_MB ]
 then
-        echo -e "\033[31mError: image edison-image-edison.ext4(${IMAGE_SIZE_MB}MB) has exceeded rootfs partition settings(${EDISON_ROOTFS_MB}MB)!\033[0m"
+        echo -e "\033[31mError: image edison-image-edison.rootfs.ext4(${IMAGE_SIZE_MB}MB) has exceeded rootfs partition settings(${EDISON_ROOTFS_MB}MB)!\033[0m"
         echo -e "\033[33mNeed to enlarge rootfs partition size, otherwise it will cause edison board bootup fail!\033[0m"
         echo -e "\033[33mYou can change it $env_dir/edison.env directly.\033[0m "
         exit 1
 fi
 
 # Copy boot partition (contains kernel and ramdisk)
-cp $build_dir/tmp/deploy/images/edison/edison-image-edison.hddimg $build_dir/toFlash/
+cp $build_dir/tmp/deploy/images/edison/edison-image-edison.rootfs.hddimg $build_dir/toFlash/
 
 # Copy u-boot
 cp $build_dir/tmp/deploy/images/edison/u-boot-edison.img $build_dir/toFlash/
@@ -62,8 +62,8 @@ do
 done
 
 # Copy rootfs
-cp $build_dir/tmp/deploy/images/edison/edison-image-edison.ext4 $build_dir/toFlash/
-cp $build_dir/tmp/deploy/images/edison/edison-image-edison.btrfs $build_dir/toFlash/
+cp $build_dir/tmp/deploy/images/edison/edison-image-edison.rootfs.ext4 $build_dir/toFlash/edison-image-edison.ext4
+cp $build_dir/tmp/deploy/images/edison/edison-image-edison.rootfs.btrfs $build_dir/toFlash/edison-image-edison.btrfs
 
 # Copy flashing script
 cp $top_repo_dir/meta-intel-edison/utils/flash/flashall.sh $build_dir/toFlash/
@@ -104,7 +104,7 @@ $mkimage_tool_path -a 0x10000 -T script -C none -n 'Edison Updater script' -d $b
 rm $build_dir/toFlash/ota_update.cmd
 
 # Generates a formatted list of all packages included in the image
-awk '{print $1 " " $3}' $build_dir/tmp/deploy/images/edison/edison-image-edison.manifest > $build_dir/toFlash/package-list.txt
+awk '{print $1 " " $3}' $build_dir/tmp/deploy/images/edison/edison-image-edison.rootfs.manifest > $build_dir/toFlash/package-list.txt
 
 echo "**** Done ***"
 echo "Files ready to flash in $build_dir/toFlash/"

commit 29817ba2186cb9ddd85c63447a21b8a459740ef4

btrfsSnapshot: fix image names now include rootfs
Yocto Nanbield changed the name of the resulting image from * to *.rootfs.
That includes edison-image-edison.rootfs.btrfs which is used by this script
to create a snapshot. btrfsFlashOta depends on the resulting 7z to have the
name edison-image-edison.snapshot.7z.

Fixes: 42556be07a32 ("postbuild: fix image names now include rootfs")
Signed-off-by: Ferry Toth <ftoth@exalondelft.nl>

utils/flash/btrfsSnapshot.sh
@@ -7,7 +7,8 @@
 
 dirs="bin boot dev etc home lib media mnt opt proc run sbin sketch sys tmp usr var srv"
 deploy_dir=tmp/deploy/images/edison
-image_name=edison-image-edison.rootfs
+out_name=edison-image-edison
+image_name=${out_name}.rootfs
 image_ext=btrfs
 initrd=core-image-minimal-initramfs-edison.cpio.gz
 
@@ -78,7 +79,7 @@ if [ -d tmpbtrfs/@new ]; then
 fi
 
 # btrfs compress the snapshot
-sudo 7za a ${deploy_dir}/${image_name}.snapshot.7z ${deploy_dir}/${image_name}.snapshot
+sudo 7za a ${deploy_dir}/${out_name}.snapshot.7z ${deploy_dir}/${image_name}.snapshot
 
 # btrfs delete the snapshot
 sudo rm ${deploy_dir}/${image_name}.snapshot